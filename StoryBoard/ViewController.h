1db2f031efdde0594d780b6723bc362be2b2682c
Jeremy Petter <jeremy.petter@gmail.com>
2015-05-13 18:03:33 -0700

    completed layout

---
+++ StoryBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryBoard
+//
+//  Created by Jeremy Petter on 2015-05-13.
+//  Copyright (c) 2015 Jeremy Petter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StoryBoard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StoryBoard
+//
+//  Created by Jeremy Petter on 2015-05-13.
+//  Copyright (c) 2015 Jeremy Petter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
